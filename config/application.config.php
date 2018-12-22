e3e16dd7653fb6c33eb476cdb98b3f0b146e736e
Judzhin Miles <info@msbios.com>
2018-12-22 16:56:01 +0200

    Lazy Composer Commiter

---
+++ config/application.config.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * If you need an environment-specific system or application configuration,
+ * there is an example in the documentation
+ * @see https://docs.zendframework.com/tutorials/advanced-config/#environment-specific-system-configuration
+ * @see https://docs.zendframework.com/tutorials/advanced-config/#environment-specific-application-configuration
+ */
+return [
+    // Retrieve list of modules used in this application.
+    'modules' => [
+        'MSBios\Guard\DeveloperTools',
+        'MSBios\Content',
+        'MSBios\Content\CPanel',
+        'MSBios\Validator',
+        'Zend\Paginator',
+        'MSBios\Hydrator',
+        'MSBios\Cache',
+        'Zend\Cache',
+        'Zend\Serializer',
+        'MSBios\Db',
+        'MSBios\Form',
+        'Zend\Db',
+        'Zend\Mvc\Plugin\Prg',
+        'Zend\Mvc\Plugin\Identity',
+        'Zend\Mvc\Plugin\FilePrg',
+        'Zend\Mvc\Plugin\FlashMessenger',
+        'Zend\Navigation',
+        'Zend\Router',
+        'Zend\Form',
+        'Zend\I18n',
+        'MSBios\Test',
+        'MSBios\I18n',
+        'MSBios\Assetic',
+        'MSBios\Widget',
+        'MSBios\Theme',
+        'MSBios\Navigation',
+        'MSBios\Application',
+        'MSBios\Resource',
+        'MSBios\Authentication',
+        'MSBios\Guard',
+        'MSBios\CPanel',
+        'MSBios\Guard\CPanel',
+        'MSBios\Guard\Resource',
+        'ZendDeveloperTools',
+    ],
+    'module_listener_options' => [
+        'module_paths' => [
+            './module',
+            './vendor',
+        ],
+        'config_glob_paths' => [
+            realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php',
+        ],
+        'config_cache_enabled' => false,
+        // 'config_cache_key' => 'application.config.cache',
+        'module_map_cache_enabled' => false,
+        // 'module_map_cache_key' => 'application.module.cache',
+        'cache_dir' => 'data/cache/',
+    ],
+];
---
+++ src/Module.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+
+namespace MSBios\Content\CPanel\Doctrine;
+
+/**
+ * Class Module
+ * @package MSBios\Content\CPanel\Doctrine
+ */
+class Module extends \MSBios\Module
+{
+    /** @const VERSION */
+    const VERSION = '1.0.0';
+
+    /**
+     * @inheritdoc
+     *
+     * @return string
+     */
+    protected function getDir()
+    {
+        return __DIR__;
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return string
+     */
+    protected function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+}
